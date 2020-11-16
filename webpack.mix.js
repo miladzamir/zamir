7acf729f56819f8f754bff0d5ac6a6b3a1b76eec
Milad Dehghan <milawddehghan@gmail.com>
2020-11-16 04:19:43 +0330

    add new style mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles('resources/css/all.css', 'public/css/all.css')
+    .styles('resources/css/custom.css', 'public/css/custom.css');
